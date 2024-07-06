ae21fb6c8b9006760e90eaa09f4df7b2a8fa8452
HimanshuKhatarkar <109038779+HimanshuKhatarkar@users.noreply.github.com>
2024-07-06 13:21:03 +0530

    Update Contact.jsx
    Changed emailid

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
           from_name: form.name,
           to_name: "Himanshu Khatarkar",
           from_email: form.email,
-          to_email: "k.himansh97@gmail.com",
+          to_email: "deepakmundhada@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
